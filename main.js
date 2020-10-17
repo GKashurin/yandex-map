0251b8296768e26e2e26b5628299a0ba7412b0cf
GKashurin <67225544+GKashurin@users.noreply.github.com>
2020-10-17 20:08:34 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -46,3 +46,23 @@ clusterer.add(geoObjects);
 }
 
 ymaps.ready(init);
+
+function addListeners(myMap) {
+myMap.events.add('click', function (e) {
+  if (!myMap.balloon.isOpen()) {
+      var coords = e.get('coords');
+      myMap.balloon.open(coords, {
+          contentHeader:'Событие!',
+          contentBody:'<p>Кто-то щелкнул по карте.</p>' +
+              '<p>Координаты щелчка: ' + [
+              coords[0].toPrecision(6),
+              coords[1].toPrecision(6)
+              ].join(', ') + '</p>',
+          contentFooter:'<sup>Щелкните еще раз</sup>'
+      });
+  }
+  else {
+      myMap.balloon.close();
+  }
+})
+};
